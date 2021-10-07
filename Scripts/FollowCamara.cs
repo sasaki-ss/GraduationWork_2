5913ce82192d31000aa6f176dada609b3f2edfa3
dooooori <binamu1215@gmail.com>
2021-10-07 11:41:39 +0900

    [add] プレイヤーが壁に当たった時反対に移動する処理を追加、カメラが追従するように処理を追加 [update] Player.prefabを更新

--- Scripts/FollowCamara.cs
+++ Scripts/FollowCamara.cs
@@ -6,12 +6,17 @@ public class FollowCamara : MonoBehaviour
 {
     [SerializeField]
     private Camera cameraObj;
-
+    private GameObject player;
+    private Transform playerTf;
+    private Transform cameraTf;
     public float bottomY { get; private set; }
 
     private void Awake()
     {
         cameraObj = this.GetComponent<Camera>();
+        player = GameObject.Find("Player");
+        playerTf = player.transform;
+        cameraTf = playerTf;
     }
 
     private void Start()
@@ -22,6 +27,8 @@ public class FollowCamara : MonoBehaviour
     private void Update()
     {
         bottomY = transform.position.y - 5f;
+
+        transform.position = new Vector3(transform.position.x, cameraTf.position.y + 3.5f, transform.position.z); ;
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
