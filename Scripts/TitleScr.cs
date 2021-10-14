8145b57f8ae17608bccd57a479f01f200812e77b
kuroneko96 <kaityou5555@gmail.com>
2021-10-14 11:44:22 +0900

    [update]垂直同期から60Fps固定

--- Scripts/TitleScr.cs
+++ Scripts/TitleScr.cs
@@ -5,10 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class TitleScr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
