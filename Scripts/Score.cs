5442712de77dbf0d598f9edc0d17539ebc5c00f9
佐々木海星 <kaityou5555@gmail.com>
2021-10-18 12:47:05 +0900

    [update]ハイスコアを追加 [update]ハイスコアをjsonファイルとして保存するような機能を追加

--- Scripts/Score.cs
+++ Scripts/Score.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+
+[System.Serializable]
+public class SaveScore
+{
+    public int score;
+}
 
 public class Score : MonoBehaviour
 {
     private static int score = 0;
+    private static int highScore = 0;
+    private string filePath;
+    private SaveScore save;
+
+    private void Awake()
+    {
+        filePath = Application.persistentDataPath + "/" + ".savedata.json";
+        save = new SaveScore();
+    }
 
     private void Start()
     {
+        Load();
+
+        highScore = save.score;
+
         score = 0;
+    }
+
+    private void OnDestroy()
+    {
+        if(score > highScore)
+        {
+            save.score = score;
+        }
+
+        Save();
     }
 
     public void AddScore(int _jumpCnt)
@@ -19,5 +49,26 @@ public class Score : MonoBehaviour
     public static int GetScore()
     {
         return score;
+    }
+
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(save);
+        StreamWriter sw = new StreamWriter(filePath);
+        sw.Write(json);
+        sw.Flush();
+        sw.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(filePath))
+        {
+            StreamReader sr = new StreamReader(filePath);
+            string data = sr.ReadToEnd();
+            sr.Close();
+
+            save = JsonUtility.FromJson<SaveScore>(data);
+        }
     }
 }
