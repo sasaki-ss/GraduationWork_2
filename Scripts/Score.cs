6c3ab6b3337b4610294146679dfc174b91b8c77d
佐々木海星 <kaityou5555@gmail.com>
2021-10-25 13:15:51 +0900

    [fix]InGameシーンにてスコアが表示されないバグの修正

--- Scripts/Score.cs
+++ Scripts/Score.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using UnityEngine.UI;
 using UnityEngine;
 
 [System.Serializable]
@@ -17,10 +18,14 @@ public class Score : MonoBehaviour
     private string filePath;
     private SaveScore save;
 
+    private Text scoreText;
+
     private void Awake()
     {
         filePath = Application.persistentDataPath + "/" + ".savedata.json";
         save = new SaveScore();
+
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
     }
 
     private void Start()
@@ -31,6 +36,11 @@ public class Score : MonoBehaviour
         isHighScore = false;
 
         score = 0;
+    }
+
+    private void Update()
+    {
+        scoreText.text = score.ToString();
     }
 
     private void OnDestroy()
