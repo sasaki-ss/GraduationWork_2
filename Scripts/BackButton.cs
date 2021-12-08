8cebe8a8d40ca5102291d59fc98474f8406aef60
dooooori <binamu1215@gmail.com>
2021-12-08 18:02:40 +0900

    [add] ボタンにシーン遷移追加

---
+++ Scripts/BackButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    string sceneName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneName = SceneManager.GetActiveScene().name;
+    }
+
+    public void Onclick()
+    {   //É{É^ÉìÇÃèàóù
+        if (sceneName == "Ranking") SceneManager.LoadScene("Title");
+        else if(sceneName == "UserLogin" && gameObject.name == "CreateButton") SceneManager.LoadScene("UserRegistration");
+        else if(sceneName == "UserLogin" && gameObject.name == "ReturnButton") SceneManager.LoadScene("Title");
+        else if(sceneName == "UserRegistration" && gameObject.name == "CreateButton") SceneManager.LoadScene("Title");
+        else if(sceneName == "UserRegistration" && gameObject.name == "ReturnButton") SceneManager.LoadScene("UserLogin");
+    }
+}
