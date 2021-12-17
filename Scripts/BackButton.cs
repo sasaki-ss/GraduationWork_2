e4d45b8103ba39e62470da294748e731a1ea2d66
dooooori <binamu1215@gmail.com>
2021-12-17 13:16:17 +0900

    [add] TitleとResultの画面にランキングなどのシーンに遷移できるようにボタンを追加

--- Scripts/BackButton.cs
+++ Scripts/BackButton.cs
@@ -18,10 +18,20 @@ public class BackButton : MonoBehaviour
     public void Onclick()
     {   //É{É^ÉìÇÃèàóù
         if (sceneName == "Ranking") SceneManager.LoadScene("Title");
+
         else if (sceneName == "UserLogin" && gameObject.name == "LoginButton") Flag = true;
         else if (sceneName == "UserLogin" && gameObject.name == "CreateButton") SceneManager.LoadScene("UserRegistration");
         else if (sceneName == "UserLogin" && gameObject.name == "ReturnButton") SceneManager.LoadScene("Title");
+
         else if (sceneName == "UserRegistration" && gameObject.name == "CreateButton") Flag = true;
         else if (sceneName == "UserRegistration" && gameObject.name == "ReturnButton") SceneManager.LoadScene("UserLogin");
+
+        else if (sceneName == "Title" && gameObject.name == "TransitionRanking") SceneManager.LoadScene("Ranking");
+        else if (sceneName == "Title" && gameObject.name == "TransitionLogin") SceneManager.LoadScene("UserLogin");
+        else if (sceneName == "Title" && gameObject.name == "TransitionInGame") SceneManager.LoadScene("InGame");
+
+        else if (sceneName == "Result" && gameObject.name == "TransitionRanking") SceneManager.LoadScene("Ranking");
+        else if (sceneName == "Result" && gameObject.name == "TransitionLogin") SceneManager.LoadScene("UserLogin");
+        else if (sceneName == "Result" && gameObject.name == "TransitionInGame") SceneManager.LoadScene("InGame");
     }
 }
